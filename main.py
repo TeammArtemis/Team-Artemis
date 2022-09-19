4988b5e56224f8fc761e02347f6b0d14576f8487
tanimsk <sktanim5800@gmail.com>
2022-09-20 00:03:31 +0600

    init

---
+++ main.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+import numpy as np
+
+
+OBJECT_AREA = 300
+
+# Define Colors in HSV
+red_lower = np.array([0, 93, 179], np.uint8)
+red_upper = np.array([0, 255, 255], np.uint8)
+
+
+green_lower = np.array([38, 200, 0], np.uint8)
+green_upper = np.array([110, 255, 255], np.uint8)
+
+
+# Capturing video through webcam
+webcam = cv2.VideoCapture(0)
+WIDTH = webcam.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+
+def direction_generator(pos_x):
+    if ((WIDTH/2) < pos_x):
+        print("left")
+    else:
+        print("right")
+
+
+# Start a while loop
+while (1):
+
+    # Reading the video from the
+    # webcam in image frames
+    _, imageFrame = webcam.read()
+
+    # Convert the imageFrame in
+    # BGR(RGB color space) to
+    # HSV(hue-saturation-value)
+    # color space
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+
+    # Set range for red color
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
+
+    # Set range for blue color and
+    # define mask
+    blue_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+
+    # Morphological Transform, Dilation
+    # for each color and bitwise_and operator
+    # between imageFrame and mask determines
+    # to detect only that particular color
+    kernal = np.ones((5, 5), "uint8")
+
+    # For red color
+    red_mask = cv2.dilate(red_mask, kernal)
+    res_red = cv2.bitwise_and(imageFrame, imageFrame,
+                              mask=red_mask)
+
+    # For blue color
+    blue_mask = cv2.dilate(blue_mask, kernal)
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame,
+                               mask=blue_mask)
+
+    # Creating contour to track red color
+    contours, hierarchy = cv2.findContours(red_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > OBJECT_AREA):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (0, 0, 255), 2)
+
+            cv2.putText(imageFrame, "Red Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                        (0, 0, 255))
+
+            direction_generator(x + w/2)
+
+    # Creating contour to track blue color
+    contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > OBJECT_AREA):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (0, 255, 0), 2)
+
+            cv2.putText(imageFrame, "Green Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1.0, (0, 255, 0))
+
+            direction_generator(x + w/2)
+
+    # Program Termination
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
